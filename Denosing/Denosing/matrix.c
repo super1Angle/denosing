fb9e90c842bb3746ebe694e5a9378022f148ee24
super1Angle <1181113723@qq.com>
2014-04-03 15:45:45 +0800

    third commit

--- Denosing/Denosing/matrix.c
+++ Denosing/Denosing/matrix.c
@@ -1,7 +1,9 @@
 #include "matrix.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
+/*这里有个内存泄露的问题*/
 double* dotSub( double* iAry1, double* iAry2, int iRows, int iCols )
 {
 	int i,j;
@@ -41,13 +43,15 @@ double* dotDiv( double* iAry1, double* iAry2, int iRows, int iCols )
 double mean( double* iAry,int iRows, int iCols )
 {
 	int i,j;
-	double sum;
+	double sum  =0.0;
 	for( i = 0; i < iRows; i++ )
-		for( j = 0; j <iCols; j++)
+		for( j = 0; j <iCols; j++){
 			if( *( iAry + i * iCols + j ) > 0 ){
 				sum +=  *( iAry +i * iCols + j );
 			}else{
 				sum +=  -*( iAry +i * iCols + j );
 			}
+		}
 	sum /= iRows * iCols;
+	return sum;
 }
--- Denosing/Denosing/tv.c
+++ Denosing/Denosing/tv.c
@@ -5,17 +5,15 @@
 int iter;   //迭代次数
 double dt;   //时间步长
 int ep;    //加入的常数值，防止奇异方差
-
+double *tmp, *tmp1;
 //声明一些偏微分
 /***************************************************/
 //int flag = 0;
 //int i, j;
 
-void tv( Img* I, Img* I0, double lam)
+void tv( double* I, double* I0, double lam)
 {
 	int i, j, m;
-	double *tmp = ( double* )malloc( width * height  * sizeof( double ) );
-	double *tmp1 = ( double* )malloc( width * height  * sizeof( double ) );
 
 	double *dx = ( double* )malloc( width * height * sizeof( double ) );   
 	double *dy = ( double* )malloc( width * height * sizeof( double ) );
@@ -26,82 +24,71 @@ void tv( Img* I, Img* I0, double lam)
 	double *Den = ( double* )malloc( width * height  * sizeof( double ) );
 	double *I_t = ( double* )malloc( width * height  * sizeof( double ) ); 
 
-	for ( i = 0; i < height; i++)
-		for( j = 0; j < width; j++)
-			*( tmp + i * width + j ) = *( tmp1 + i * width + j ) = *( I->data + i * width + j ) * 1.0;
-
 	for( m = 0; m < iter; m++){
-		//if( flag == 0 ){
-			for( i = 0; i < I->height; i++)
-				for( j = 0; j < I->width; j++){
+			for( i = 0; i < height; i++)
+				for( j = 0; j < width; j++){
 					//求dx
 					if( j == 0){
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + 1) -  *( tmp + i * I->width ) ) / 2;
-					}else if( j == I->width - 1 ){
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + I->width - 1) - *( tmp + i * I->width + I->width - 2) ) / 2;
+						*( dx + i * width + j) = ( *( tmp + i * width + 1) -  *( tmp + i * width ) ) / 2;
+					}else if( j == width - 1 ){
+						*( dx + i * width + j) = ( *( tmp + i * width + width - 1) - *( tmp + i * width + width - 2) ) / 2;
 					}else{
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + ( j + 1 ) ) -  *( tmp + i * I->width + ( j - 1) ) ) / 2;
+						*( dx + i * width + j) = ( *( tmp + i * width + ( j + 1 ) ) -  *( tmp + i * width + ( j - 1) ) ) / 2;
 					}
 					//求dy
 					if( i == 0 ){
-						*( dy + i * I->width + j) = ( *( tmp + 1 * I->width + j ) - *( tmp + j ) ) / 2;
-					}else if( i == I->height - 1 ){
-						*( dy + i * I->width + j) = ( *( tmp + (I->height - 1) * I->width + j) - *( tmp + (I->height -2) * I->width  + j )) / 2;
+						*( dy + i * width + j) = ( *( tmp + 1 * width + j ) - *( tmp + j ) ) / 2;
+					}else if( i == height - 1 ){
+						*( dy + i * width + j) = ( *( tmp + (height - 1) * width + j) - *( tmp + (height -2) * width  + j )) / 2;
 					}else{
-						*( dy + i * I->width + j) = ( *( tmp + ( i + 1 ) * I->width + j ) - *( tmp + ( i - 1 ) * I->width + j ) ) / 2;
+						*( dy + i * width + j) = ( *( tmp + ( i + 1 ) * width + j ) - *( tmp + ( i - 1 ) * width + j ) ) / 2;
 					}
 					//求dxx
 					if( j == 0 ){
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + 1) - *( tmp + i * I->width ) ;
-					}else if( j == I->width - 1 ){
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + I->width - 2) - *( tmp + i * I->width + I->width - 1 ) ;
+						*( dxx + i * width + j) = *( tmp + i * width + 1) - *( tmp + i * width ) ;
+					}else if( j == width - 1 ){
+						*( dxx + i * width + j) = *( tmp + i * width + width - 2) - *( tmp + i * width + width - 1 ) ;
 					}else{
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + ( j + 1 ) ) +  *( tmp + i * I->width + ( j - 1) ) - 2 * ( *( tmp + i * I->width + j));
+						*( dxx + i * width + j) = *( tmp + i * width + ( j + 1 ) ) +  *( tmp + i * width + ( j - 1) ) - 2 * ( *( tmp + i * width + j));
 					}
 					//求dyy
 					if( i == 0){
-						*( dyy + i * I->width + j) = *( tmp + 1 * I->width + j ) - *( tmp + j );
-					}else if( i == I->height - 1 ){
-						*( dyy + i * I->width + j) = *( tmp + (I->height - 2) * I->width + j) - *( tmp + (I->height -1) * I->width  + j );
+						*( dyy + i * width + j) = *( tmp + 1 * width + j ) - *( tmp + j );
+					}else if( i == height - 1 ){
+						*( dyy + i * width + j) = *( tmp + (height - 2) * width + j) - *( tmp + (height -1) * width  + j );
 					}else{
-						*( dyy + i * I->width + j) = *( tmp + ( i + 1 ) * I->width + j ) + *( tmp + ( i - 1 ) * I->width + j ) - 2 *( *( tmp + i * I->width + j ) );
+						*( dyy + i * width + j) = *( tmp + ( i + 1 ) * width + j ) + *( tmp + ( i - 1 ) * width + j ) - 2 *( *( tmp + i * width + j ) );
 					}
 				}//到这里为止，已经把偏导数给求好了
 				//这里求dxdy
-				for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
+				for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
 						if( j == 0 ){
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + 1 ) - *( dy + i * I->width ) ) / 2;
-						}else if( j == I->width - 1 ){
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + I->width - 1 ) - *( dy + i * I->width + I->width - 2 ) ) / 2;
+							*( dxy + i * width + j ) = ( *( dy + i * width + 1 ) - *( dy + i * width ) ) / 2;
+						}else if( j == width - 1 ){
+							*( dxy + i * width + j ) = ( *( dy + i * width + width - 1 ) - *( dy + i * width + width - 2 ) ) / 2;
 						}else{
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + j + 1 ) - *( dy + i * I->width + j -1 ) ) / 2;
+							*( dxy + i * width + j ) = ( *( dy + i * width + j + 1 ) - *( dy + i * width + j -1 ) ) / 2;
 						}
 				}
 				//这里求Num,Den
-				for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
-						*( Num + i * I->width + j ) = ( *( dxx + i * I->width + j ) ) * ( ep + pow( *( dy + i * I->width + j), 2) ) + 
-							( *( dyy + i * I->width + j ) ) * ( ep + pow( *( dx + i * I->width + j), 2) ) - 2 * ( *( dx + i * I->width + j)) * ( *( dy + i * I->width + j)) *  ( *( dxy + i * I->width + j));
-						*(Den + i * I->width + j ) = pow( (ep + pow( *(dx + i * I->width + j), 2) + pow( *(dy + i * I->width + j), 2) ) , 1.5);
+				for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
+						*( Num + i * width + j ) = ( *( dxx + i * width + j ) ) * ( ep + pow( *( dy + i * width + j), 2) ) + 
+							( *( dyy + i * width + j ) ) * ( ep + pow( *( dx + i * width + j), 2) ) - 2 * ( *( dx + i * width + j)) * ( *( dy + i * width + j)) *  ( *( dxy + i * width + j));
+						*(Den + i * width + j ) = pow( (ep + pow( *(dx + i * width + j), 2) + pow( *(dy + i * width + j), 2) ) , 1.5);
 					}
 
 		//这里进行迭代运算
-		for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
-						*( I_t + i * I->width + j ) = (*( Num + i * I->width + j )) / (*( Den + i * I->width + j )) +  0 * (*( tmp1 + i * I->width + j) - *( tmp + i * I->width + j) );
+		for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
+						*( I_t + i * width + j ) = (*( Num + i * width + j )) / (*( Den + i * width + j )) +  lam * (*( tmp1 + i * width + j) - *( tmp + i * width + j) );
 					}
-		for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
-						*( tmp + i * I->width + j ) += dt * (*( I_t + i * I->width + j) );
+		for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
+						*( tmp + i * width + j ) += dt * (*( I_t + i * width + j) );
 					}
 	}
-	for( i = 0; i < I->height; i++)
-		for( j = 0; j < I->width; j++){
-			*(I->data + i * width + j ) = *( tmp + i * width + j );
-		}
-	//}
-//	flag++;	
 
 		free( dx );
 		free( dy );
@@ -111,17 +98,13 @@ void tv( Img* I, Img* I0, double lam)
 		free( Num );
 		free( Den );
 		free( I_t );
-		free( tmp );
-		free( tmp1 );
 
 }
 
-double calc_lam( Img* I, Img* I0)
+double calc_lam( double* tmp, double* tmp1)
 {
 	int i, j;
 	double lam = 0.0, lam1 = 0.0, lam2 = 0.0;
-	double *tmp = ( double* )malloc( width * height  * sizeof( double ) );
-	double *tmp1 = ( double* )malloc( width * height  * sizeof( double ) );
 
 	double *dx = ( double* )malloc( width * height * sizeof( double ) );   
 	double *dy = ( double* )malloc( width * height * sizeof( double ) );
@@ -130,74 +113,70 @@ double calc_lam( Img* I, Img* I0)
 	double *dxy = ( double* )malloc( width * height  * sizeof( double ) );
 	double *Num = ( double* )malloc( width * height  * sizeof( double ) );
 	double *Den = ( double* )malloc( width * height  * sizeof( double ) );
-
-	for ( i = 0; i < height; i++)
-		for( j = 0; j < width; j++)
-			*( tmp + i * width + j ) = *( tmp1 + i * width + j ) = *( I->data + i * width + j ) * 1.0;
 	
-	for( i = 0; i < I->height; i++)
-				for( j = 0; j < I->width; j++){
+	for( i = 0; i < height; i++)
+				for( j = 0; j < width; j++){
 					//求dx
 					if( j == 0){
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + 1) -  *( tmp + i * I->width ) ) / 2;
-					}else if( j == I->width - 1 ){
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + I->width - 1) - *( tmp + i * I->width + I->width - 2) ) / 2;
+						*( dx + i * width + j) = ( *( tmp + i * width + 1) -  *( tmp + i * width ) ) / 2;
+					}else if( j == width - 1 ){
+						*( dx + i * width + j) = ( *( tmp + i * width + width - 1) - *( tmp + i * width + width - 2) ) / 2;
 					}else{
-						*( dx + i * I->width + j) = ( *( tmp + i * I->width + ( j + 1 ) ) -  *( tmp + i * I->width + ( j - 1) ) ) / 2;
+						*( dx + i * width + j) = ( *( tmp + i * width + ( j + 1 ) ) -  *( tmp + i * width + ( j - 1) ) ) / 2;
 					}
 					//求dy
 					if( i == 0 ){
-						*( dy + i * I->width + j) = ( *( tmp + 1 * I->width + j ) - *( tmp + j ) ) / 2;
-					}else if( i == I->height - 1 ){
-						*( dy + i * I->width + j) = ( *( tmp + (I->height - 1) * I->width + j) - *( tmp + (I->height -2) * I->width  + j )) / 2;
+						*( dy + i * width + j) = ( *( tmp + 1 * width + j ) - *( tmp + j ) ) / 2;
+					}else if( i == height - 1 ){
+						*( dy + i * width + j) = ( *( tmp + (height - 1) * width + j) - *( tmp + (height -2) * width  + j )) / 2;
 					}else{
-						*( dy + i * I->width + j) = ( *( tmp + ( i + 1 ) * I->width + j ) - *( tmp + ( i - 1 ) * I->width + j ) ) / 2;
+						*( dy + i * width + j) = ( *( tmp + ( i + 1 ) * width + j ) - *( tmp + ( i - 1 ) * width + j ) ) / 2;
 					}
 					//求dxx
 					if( j == 0 ){
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + 1) - *( tmp + i * I->width ) ;
-					}else if( j == I->width - 1 ){
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + I->width - 2) - *( tmp + i * I->width + I->width - 1 ) ;
+						*( dxx + i * width + j) = *( tmp + i * width + 1) - *( tmp + i * width ) ;
+					}else if( j == width - 1 ){
+						*( dxx + i * width + j) = *( tmp + i * width + width - 2) - *( tmp + i * width + width - 1 ) ;
 					}else{
-						*( dxx + i * I->width + j) = *( tmp + i * I->width + ( j + 1 ) ) +  *( tmp + i * I->width + ( j - 1) ) - 2 * ( *( tmp + i * I->width + j));
+						*( dxx + i * width + j) = *( tmp + i * width + ( j + 1 ) ) +  *( tmp + i * width + ( j - 1) ) - 2 * ( *( tmp + i * width + j));
 					}
 					//求dyy
 					if( i == 0){
-						*( dyy + i * I->width + j) = *( tmp + 1 * I->width + j ) - *( tmp + j );
-					}else if( i == I->height - 1 ){
-						*( dyy + i * I->width + j) = *( tmp + (I->height - 2) * I->width + j) - *( tmp + (I->height -1) * I->width  + j );
+						*( dyy + i * width + j) = *( tmp + 1 * width + j ) - *( tmp + j );
+					}else if( i == height - 1 ){
+						*( dyy + i * width + j) = *( tmp + (height - 2) * width + j) - *( tmp + (height -1) * width  + j );
 					}else{
-						*( dyy + i * I->width + j) = *( tmp + ( i + 1 ) * I->width + j ) + *( tmp + ( i - 1 ) * I->width + j ) - 2 *( *( tmp + i * I->width + j ) );
+						*( dyy + i * width + j) = *( tmp + ( i + 1 ) * width + j ) + *( tmp + ( i - 1 ) * width + j ) - 2 *( *( tmp + i * width + j ) );
 					}
 				}//到这里为止，已经把偏导数给求好了
 				//这里求dxdy
-				for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
+				for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
 						if( j == 0 ){
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + 1 ) - *( dy + i * I->width ) ) / 2;
-						}else if( j == I->width - 1 ){
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + I->width - 1 ) - *( dy + i * I->width + I->width - 2 ) ) / 2;
+							*( dxy + i * width + j ) = ( *( dy + i * width + 1 ) - *( dy + i * width ) ) / 2;
+						}else if( j == width - 1 ){
+							*( dxy + i * width + j ) = ( *( dy + i * width + width - 1 ) - *( dy + i * width + width - 2 ) ) / 2;
 						}else{
-							*( dxy + i * I->width + j ) = ( *( dy + i * I->width + j + 1 ) - *( dy + i * I->width + j -1 ) ) / 2;
+							*( dxy + i * width + j ) = ( *( dy + i * width + j + 1 ) - *( dy + i * width + j -1 ) ) / 2;
 						}
 				}
 				//这里求Num,Den
-				for( i = 0; i < I->height; i++)
-					for( j = 0; j < I->width; j++){
-						*( Num + i * I->width + j ) = ( *( dxx + i * I->width + j ) ) * ( ep + pow( *( dy + i * I->width + j), 2) ) + 
-							( *( dyy + i * I->width + j ) ) * ( ep + pow( *( dx + i * I->width + j), 2) ) - 2 * ( *( dx + i * I->width + j)) * ( *( dy + i * I->width + j)) *  ( *( dxy + i * I->width + j));
-						*(Den + i * I->width + j ) = pow( (ep + pow( *(dx + i * I->width + j), 2) + pow( *(dy + i * I->width + j), 2) ) , 1.5);
+				for( i = 0; i < height; i++)
+					for( j = 0; j < width; j++){
+						*( Num + i * width + j ) = ( *( dxx + i * width + j ) ) * ( ep + pow( *( dy + i * width + j), 2) ) + 
+							( *( dyy + i * width + j ) ) * ( ep + pow( *( dx + i * width + j), 2) ) - 2 * ( *( dx + i * width + j)) * ( *( dy + i * width + j)) *  ( *( dxy + i * width + j));
+						*(Den + i * width + j ) = pow( (ep + pow( *(dx + i * width + j), 2) + pow( *(dy + i * width + j), 2) ) , 1.5);
 					}
 
 						//这里计算保真度项系数lam
-						for( i = 0; i < I->height; i++)
-							for( j = 0; j < I->width; j++)
-								lam1 += *( Num + i * I->width + j ) / *( Den + i * I->width + j ) * ( *( tmp + i * I->width + j ) - *( tmp1 + i * I->width + j) );
+						for( i = 0; i < height; i++)
+							for( j = 0; j < width; j++)
+								lam1 += *( Num + i * width + j ) / *( Den + i * width + j ) * ( *( tmp + i * width + j ) - *( tmp1 + i * width + j) );
 						lam1 /= width * height;
 
-						for( i = 0; i < I->height; i++)
-							for( j = 0; j < I->width; j++)
-								lam2 += pow( *( tmp + i * I->width + j ) - *( tmp1 + i * I->width + j ), 2);
+						for( i = 0; i < height; i++)
+							for( j = 0; j < width; j++)
+								lam2 += pow( *( tmp + i * width + j ) - *( tmp1 + i * width + j ), 2);
 						lam2 /= width *height;
 
 						lam = lam1 / lam2;     //第一次计算时，
@@ -209,8 +188,6 @@ double calc_lam( Img* I, Img* I0)
 		free( dyy );
 		free( Num );
 		free( Den );
-		free( tmp );
-		free( tmp1 );
 
 		return lam;
 }
@@ -219,17 +196,54 @@ void tv_demo( Img* I, Img* I0)
 {
 	double lam;
 	double delt;     //计算前后两次迭代结果的误差大小
-	Img* old;
+	int i, j;
+	double* old;
+	double threshold = 0.01;
 
+	int  times = 0;      //记录循环执行的次数
 
+	tmp = ( double* )malloc( width * height  * sizeof( double ) );
+	tmp1 = ( double* )malloc( width * height  * sizeof( double ) );
+	old = ( double* )malloc( width * height  * sizeof( double ) );
 
+	for ( i = 0; i < height; i++)
+		for( j = 0; j < width; j++)
+			*( tmp + i * width + j ) = *( tmp1 + i * width + j ) = *( I->data + i * width + j ) * 1.0;
+
+	for ( i = 0; i < height; i++)
+		for( j = 0; j < width; j++)
+			*( old + i * width + j ) = 0.0;
+
+	delt = 1.0;  //这是随便设置的一个值，主要是为了能进入循环
 	iter = 80;   //迭代次数
 	dt = 0.2;   //时间步长
 	ep = 1;    //加入的常数值，防止奇异方差
+	lam = 0.0;
 
-	while( mean( dotSub( I->data, old->data, height, width )) > ){
-		
+	
+
+	while( delt > threshold /*TRUE*/){
+		for ( i = 0; i < height; i++)
+			for( j = 0; j < width; j++)
+				*( old + i * I->width + j )= *( tmp+ i * width + j );
+		tv( tmp, tmp1, lam );
+		lam = calc_lam( tmp, tmp1 );
+		//printf( "%f  \n", lam );
+		delt = mean( dotSub( tmp, old, height, width ), height, width );
+		//printf( "%f  \n",delt );
+		times++;
 	}
+
+	printf( "%d", times );
+
+	for( i = 0; i < I->height; i++)
+		for( j = 0; j < I->width; j++){
+			*(I->data + i * width + j ) = *( tmp + i * width + j );
+		}
+
+		free( tmp );
+		free( tmp1 );
+		free( old );
 }
 
 	
